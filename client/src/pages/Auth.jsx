3f7f7e2045cac343be4699add534e761a2eb012b
adityakr0660 <adityahariomofficial@gmail.com>
2026-07-21 20:31:02 +0530

    update home page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,89 +1,90 @@
-import React from 'react'
+import React from "react";
 import { VscRobot } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 import { FcGoogle } from "react-icons/fc";
-import { linkWithCredential, signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../utils/firebase';
-import { serverUrl } from '../App';
-import axios from 'axios'
-import { useDispatch } from 'react-redux';
-import { setUserData } from '../redux/userSlice';
-import { useNavigate } from 'react-router-dom';
+import { linkWithCredential, signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../utils/firebase";
+import { serverUrl } from "../App";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
+import { useNavigate } from "react-router-dom";
 
-function Auth({isModel = false}) {
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+function Auth({ isModel = false }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleGoogleAuth = async () => {
     try {
-      const response = await signInWithPopup(auth, provider)
-      let User = response.user
-      let name = User.displayName
-      let email = User.email
-      const result = await axios.post(serverUrl + "/api/auth/google",
-        { name, email }, { withCredentials: true }
-      )
-      dispatch(setUserData(result.data))
-      navigate("/")
-
+      const response = await signInWithPopup(auth, provider);
+      let User = response.user;
+      let name = User.displayName;
+      let email = User.email;
+      const result = await axios.post(
+        serverUrl + "/api/auth/google",
+        { name, email },
+        { withCredentials: true },
+      );
+      dispatch(setUserData(result.data));
+      navigate("/");
     } catch (error) {
       // Don't log if user simply closed the popup
-      if (error.code !== 'auth/popup-closed-by-user') {
-        console.log(error)
+      if (error.code !== "auth/popup-closed-by-user") {
+        console.log(error);
       }
-      dispatch(setUserData(null))
+      dispatch(setUserData(null));
     }
-  }
-
+  };
 
   return (
-    <div className='w-full min-h-screen bg-[#f3f3f3] flex items-center
-    justify-center px-6 py-20' >
+    <div
+      className={`
+      w-full 
+      ${isModel ? "py4" : "min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20"}
+    `}
+    >
       <motion.div
         initial={{ opacity: 0, y: -40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.05 }}
-
-        className='w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl
-        border border-gray-200'>
-        <div className='flex items-center justify-center gap-3 mb-6'>
-          <div className='bg-black text-white p-2 rounded-lg'>
+        className={`
+          w-full 
+          ${isModel ? "max-w-md p-8 rounded-3xl" : "max-w-lg p-12 rounded-[32px]"}
+        bg-white shadow-2xl border vorder-gray-200`}
+      >
+        <div className="flex items-center justify-center gap-3 mb-6">
+          <div className="bg-black text-white p-2 rounded-lg">
             <VscRobot size={20} />
-
           </div>
-          <h2 className='font-semibold text-lg'>InterviewIQ.AI</h2>
+          <h2 className="font-semibold text-lg">InterviewIQ.AI</h2>
         </div>
-        <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>
-          Continue with {" "}
-          <span className='bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2'>
+        <h1 className="text-2xl md:text-3xl font-semibold text-center leading-snug mb-4">
+          Continue with{" "}
+          <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2">
             <IoSparkles size={20} />
             AI Smart Interview
           </span>
         </h1>
 
-        <p className='text-gray-500 text-center text-sm md:text-base leading-relaxed mb-8'>
-          Sign in to start AI-powered mock Interviews,
-          track your progress, and unlock detailed performance insights.
+        <p className="text-gray-500 text-center text-sm md:text-base leading-relaxed mb-8">
+          Sign in to start AI-powered mock Interviews, track your progress, and
+          unlock detailed performance insights.
         </p>
 
         <motion.button
           onClick={handleGoogleAuth}
           whileHover={{ opacity: 0.9, scale: 1.03 }}
           whileTap={{ opacity: 1, scale: 0.98 }}
-
-          className='w-full flex items-center justify-center
-            gap-3 py-3 bg-black text-white rounded-full shadow-md'>
+          className="w-full flex items-center justify-center
+            gap-3 py-3 bg-black text-white rounded-full shadow-md"
+        >
           <FcGoogle size={20} />
           Continue with Google
-
         </motion.button>
-
       </motion.div>
-
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
